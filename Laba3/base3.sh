f51442c53f4c9501abffcedc79f4085261db8dd5
DarkDuck <kksenialukianenko96@gmail.com>
2023-11-01 21:04:17 +0300

    fixed

--- Laba3/base3.sh
+++ Laba3/base3.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 #$1 $2 $3 $4 $5
 
-if [[$# -eq5]]; then
-	echo 'колтчество параметров не соответствует. Ожидается 5 параметров,\n bash3.sh [options = 5] \n example bash3.sh group1 group2 user1 user2 user 3 \n'
+#!/bin/bash
+
+# Check if the number of arguments is not equal to 5
+if [ $# -ne 5 ]; then
+    echo "Number of parameters doesn't match. Expected 5 parameters."
+    echo "Usage: bash3.sh group1 group2 user1 user2 user3"
+    exit 1  # Exit the script
 fi
-groupadd $1 $2
-adduser $3 $4 $5
+
+# Create groups
+groupadd $1
+groupadd $2
+
+# Create users and add them to groups
+adduser $3 
+adduser $4  
+adduser $5  
+# Add users to their respective groups
 gpasswd -a $3 $1
 gpasswd -a $4 $1
-grasswd -a $4 $2 
-grasswd -a $5 $2
-for i in $(seq 1 3)
-do 
-	echo 'polzovatel user$i'
-	id user$i
+gpasswd -a $4 $2
+gpasswd -a $5 $2
+# Use a for loop to iterate from 1 to 3 and display user information
+for i in $(seq 1 3); do
+    echo "polzovatel user$i"
+    id user$i
 done
-mkdir /srv/dir1 /srv/dir2
-chgrp group1 /srv/dir1
-chgrp group2 /srv/dir2
+
+# Create directories and set group and permissions
+mkdir -p /srv/dir1 /srv/dir2
+chgrp $1 /srv/dir1
+chgrp $2 /srv/dir2
 chmod 2775 /srv/dir1 /srv/dir2
+
+# List the contents of /srv
 ls -la /srv
+
